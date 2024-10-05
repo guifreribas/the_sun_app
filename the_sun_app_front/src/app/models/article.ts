b5bcf130407d2fcf85a11f646cdbe5754711a172
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-06 00:11:41 +0200

    refactor Article interface and main component; enhance article display with select functionality and improve service response type

--- the_sun_app_front/src/app/models/article.ts
+++ the_sun_app_front/src/app/models/article.ts
@@ -1,9 +1,13 @@
 export interface Article {
-    id: string;
-    title: string;
-    description: string;
-    url: string;
-    urlToImage: string;
-    createdAt: Date;
-    deletedAt?: Date;  
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  createdAt: Date;
+}
+
+export interface GetArticlesResponse {
+  results: Article[];
+  message: string;
 }
--- the_sun_app_front/src/app/pages/main/main.component.ts
+++ the_sun_app_front/src/app/pages/main/main.component.ts
@@ -1,15 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
 import { firstValueFrom } from 'rxjs';
+import { Article, GetArticlesResponse } from '../../models/article';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+const muckData = [
+  {
+    p1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+    p2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+  },
+  {
+    p1: 'Lorem seconddolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+    p2: 'Lorem ipsum second, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+  },
+  {
+    p1: 'Third Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+    p2: 'Lorem ipsum dolor sit amet, third third consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+  },
+  {
+    p1: 'Fourth Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+    p2: 'Lorem ipsum dolor sit amet, fourth. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisi velit.',
+  },
+];
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  @ViewChild('button1') button1: any;
+  @ViewChild('button2') button2: any;
+  @ViewChild('button3') button3: any;
+  @ViewChild('button4') button4: any;
+
+  public article: Article | undefined;
+  public articles: Article[] | undefined;
+  public selector = new FormControl();
+
+  public muckData = muckData;
+  public muckDataIndex = 0;
+
   private articleService = inject(ArticleService);
 
   ngOnInit() {
@@ -18,14 +53,21 @@ export class MainComponent {
   }
 
   async getArticle() {
-    const articles = await firstValueFrom(
+    const article = await firstValueFrom(
       this.articleService.getArticleById('67018cedb4a20650ebfb6991')
     );
-    console.log(articles);
+    this.article = article;
+    console.log(article);
   }
 
   async getArticles() {
     const articles = await firstValueFrom(this.articleService.getArticles());
+    this.articles = articles.results;
     console.log(articles);
+  }
+
+  onSelectTarget(target: number) {
+    console.log(target);
+    this.muckDataIndex = target - 1;
   }
 }
