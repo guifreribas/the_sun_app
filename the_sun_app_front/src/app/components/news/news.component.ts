6907eb5f0aa69f4ae1261e89dabaa8b658c9ce5f
deuvede24 <cdaniela.vd@gmail.com>
2024-10-06 01:37:23 +0200

    news okish

--- the_sun_app_front/src/app/components/news/news.component.ts
+++ the_sun_app_front/src/app/components/news/news.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ArticleService } from '../../services/article.service';
+import { Article } from '../../models/article';
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './news.component.html',
-  styleUrl: './news.component.scss'
+  styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  articles: Article[] = [];
 
+  private articleService = inject(ArticleService);
+
+  ngOnInit() {
+    this.articleService.getArticles().subscribe({
+      next: (response) => {
+        this.articles = response.results;
+      },
+      error: (error) => {
+        console.error('Error fetching articles:', error);
+      }
+    });
+  }
 }
