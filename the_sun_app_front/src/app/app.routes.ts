87e5ae7e6320f649577ccb61a9b2270a13f6d2c5
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-06 00:55:08 +0200

    feat(routes): add About page route and link to navbar; create About component with styling and content

--- the_sun_app_front/src/app/app.routes.ts
+++ the_sun_app_front/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { MainComponent } from './pages/main/main.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'article/:id', component: ArticleComponent },
   { path: 'main', component: MainComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' },
 ];
