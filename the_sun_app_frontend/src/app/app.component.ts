d117c91cf1a2c6c4cbc270af353fd2f5ed612ab2
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-05 14:18:48 +0200

    feat: update AppComponent to include Navbar and Footer components for improved layout structure

--- the_sun_app_frontend/src/app/app.component.ts
+++ the_sun_app_frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, NavbarComponent, FooterComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'chase_the_sun';
+	title = "The Sun App";
 }
