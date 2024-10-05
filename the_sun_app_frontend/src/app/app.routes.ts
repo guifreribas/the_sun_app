715e721826ede02cbb64f1b088900e6b8411c6d7
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-05 14:17:04 +0200

    feat: add routing for HomeComponent and ArticleComponent with dynamic article ID to enhance navigation flow

--- the_sun_app_frontend/src/app/app.routes.ts
+++ the_sun_app_frontend/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { ArticleComponent } from "./pages/article/article.component";
 
-export const routes: Routes = [{ path: "", component: HomeComponent }];
+export const routes: Routes = [
+	{ path: "", component: HomeComponent },
+	{ path: "home", component: HomeComponent },
+	{ path: "article/:id", component: ArticleComponent },
+];
