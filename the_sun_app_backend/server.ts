f3f6743c8b39ef49dd1de34636aee1375922aed9
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-10-06 00:15:36 +0200

    changed postman collection

--- the_sun_app_backend/server.ts
+++ the_sun_app_backend/server.ts
@@ -3,15 +3,15 @@ import express from "express";
 import logger from "morgan";
 import connectToMongoDB from "./src/DB/connectToMongo.js";
 import cors from "cors";
-import { PORT, APP_ORIGIN } from "./src/constants/env.js";
+import { PORT } from "./src/constants/env.js";
 import articleRouter from "./src/routes/article.js";
 
 const app = express();
 
-const ALLOWED_ORIGINS = [
-  `http://localhost:${PORT}`,
-  `http://localhost:${APP_ORIGIN}`,
-];
+// const ALLOWED_ORIGINS = [
+//   `http://localhost:${PORT}`,
+//   `http://localhost:${APP_ORIGIN}`,
+// ];
 const METHODS = ["GET", "POST"];
 
 //middlewares
@@ -20,16 +20,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (
-        typeof origin === "string" &&
-        (ALLOWED_ORIGINS.indexOf(origin) !== -1 || !origin)
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: '*',
     methods: METHODS,
   })
 );
