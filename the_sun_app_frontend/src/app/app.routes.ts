70db4fffe25226255a986276ae12e14f65d14655
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-05 19:55:09 +0200

    chore: add MainComponent route and redirect for improved navigation in the sun app

--- the_sun_app_frontend/src/app/app.routes.ts
+++ the_sun_app_frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { ArticleComponent } from "./pages/article/article.component";
+import { MainComponent } from "./pages/main/main.component";
 
 export const routes: Routes = [
 	{ path: "", component: HomeComponent },
 	{ path: "home", component: HomeComponent },
 	{ path: "article/:id", component: ArticleComponent },
+	{ path: "main", component: MainComponent },
+	{ path: "**", redirectTo: "" },
 ];
