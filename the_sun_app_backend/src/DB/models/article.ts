f3abdc0f00759b7cebaf826e28f4a2ca646c6118
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-10-05 16:48:33 +0200

    updated article model & created getAll services

--- the_sun_app_backend/src/DB/models/article.ts
+++ the_sun_app_backend/src/DB/models/article.ts
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
+//crear documento 
+
+interface ArticleDocument extends mongoose.Document{
+    id: mongoose.Types.ObjectId,
+    title: String,
+    description: String,
+    url: String,
+    urlToImage: String,
+    createdAt: Date,
+    deletedAt: Date,
+}
+
 const articleSchema = new mongoose.Schema({
     id: {type: mongoose.Types.ObjectId, required: true},
     title: { type: String, required: true},
@@ -12,5 +24,7 @@ const articleSchema = new mongoose.Schema({
 { timestamps: true }
 );
 
-const articleModel = mongoose.model("Article", articleSchema);
+
+//crear modelo con documento y esquema
+const articleModel = mongoose.model<ArticleDocument>("Article", articleSchema);
 export default articleModel;
