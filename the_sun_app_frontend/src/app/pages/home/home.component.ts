6422fe7598b3f46570bd9bf820f2f601dd2e829f
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-05 14:18:11 +0200

    feat: replace static content in HomeComponent with dynamic components for improved structure and functionality

--- the_sun_app_frontend/src/app/pages/home/home.component.ts
+++ the_sun_app_frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WelcomeComponent } from "../../components/welcome/welcome.component";
+import { NewsComponent } from "../../components/news/news.component";
+import { SearchComponent } from "../../components/search/search.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: "app-home",
+	standalone: true,
+	imports: [WelcomeComponent, NewsComponent, SearchComponent],
+	templateUrl: "./home.component.html",
+	styleUrl: "./home.component.scss",
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
