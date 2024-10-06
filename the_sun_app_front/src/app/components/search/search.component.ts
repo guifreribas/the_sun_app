81fdef8914a47521c71c1c1403d33d5d77253848
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-06 10:21:49 +0200

    Add Flowbite library for enhanced datepicker functionality and update search component layout for better UX

--- the_sun_app_front/src/app/components/search/search.component.ts
+++ the_sun_app_front/src/app/components/search/search.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  OnInit,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Datepicker, DatepickerOptions, initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit, AfterViewInit {
+  @ViewChild('datepicker', { static: true }) datepickerRef!: ElementRef;
+  public datePicker!: Datepicker;
+  public date = new FormControl();
 
+  ngOnInit() {
+    setTimeout(() => {
+      initFlowbite();
+    }, 100);
+
+    const options: DatepickerOptions = {
+      defaultDatepickerId: null,
+      autohide: false,
+      format: 'mm/dd/yyyy',
+      maxDate: new Date().toISOString(),
+      minDate: null,
+      orientation: 'bottom',
+      buttons: false,
+      title: null,
+      rangePicker: false,
+      onShow: () => {},
+      onHide: () => {},
+    };
+
+    this.datePicker = new Datepicker(this.datepickerRef.nativeElement, options);
+    console.log('DatePicker', this.datePicker);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Date Picker ref', this.datepickerRef.nativeElement);
+  }
+
+  dateChange() {
+    console.log('Date change', this.datePicker);
+    console.log(this.datePicker.getDate());
+  }
 }
--- the_sun_app_front/tailwind.config.js
+++ the_sun_app_front/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
